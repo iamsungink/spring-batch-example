77c8ed96f30b68582875645304abff791f696560
iamsungink <iamsungink@naver.com>
2021-09-14 22:24:53 +0900

    ch03.07. JPA 데이터 읽기

--- src/main/resources/person.sql
+++ src/main/resources/person.sql
@@ -1,13 +1,13 @@
-create table person (
+create table Person (
     id bigint primary key auto_increment,
     name varchar(255),
     age varchar (255),
     address varchar (255)
 );
 
-insert into person(name, age, address)
+insert into Person(name, age, address)
 values ('김성인','41','인천');
-insert into person(name, age, address)
+insert into Person(name, age, address)
 values ('홍길동','30','서울');
-insert into person(name, age, address)
+insert into Person(name, age, address)
 values ('아무개','25','강원');
