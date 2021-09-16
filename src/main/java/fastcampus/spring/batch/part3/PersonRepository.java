cb80121ab1dfb0595b661bcbd4c27d5341e9ec34
iamsungink <iamsungink@naver.com>
2021-09-16 18:57:41 +0900

    ch03.14. 테스트 코드 작성하기

---
+++ src/main/java/fastcampus/spring/batch/part3/PersonRepository.java
@@ -0,0 +1,8 @@
+package fastcampus.spring.batch.part3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+}
