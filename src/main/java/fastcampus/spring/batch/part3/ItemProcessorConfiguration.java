4beb89eb7a17ba9b21a5aca76d70fe9fc892f5f7
iamsungink <iamsungink@naver.com>
2021-09-16 10:01:33 +0900

    ch03.12. ItemProcessor interface 구조이해, 과제 요구사항 설명

---
+++ src/main/java/fastcampus/spring/batch/part3/ItemProcessorConfiguration.java
@@ -0,0 +1,76 @@
+package fastcampus.spring.batch.part3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Slf4j
+public class ItemProcessorConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public ItemProcessorConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job itemProcessorJob(){
+        return this.jobBuilderFactory.get("itemProcessorJob")
+                .incrementer(new RunIdIncrementer())
+                .start(this.itemProcessorStep())
+                .build();
+    }
+
+    private Step itemProcessorStep() {
+        return this.stepBuilderFactory.get("itemProcessorStep")
+                .<Person, Person>chunk(10)
+                .reader(this.itemReader())
+                .processor(this.itemProcessor())
+                .writer(this.itemWriter())
+                .build();
+    }
+
+    private ItemWriter<? super Person> itemWriter() {
+        return items -> {
+            items.forEach(x-> log.info("PERSON.ID : {}", x.getId()));
+        };
+    }
+
+    private ItemProcessor<? super Person, ? extends Person> itemProcessor() {
+        return item -> {
+            if(item.getId() % 2 == 0 ){
+                return item;
+            }
+
+            return null;
+        };
+    }
+
+    private ItemReader<Person> itemReader() {
+        return new CustomIteamReader<>(this.getItems());
+    }
+
+    private List<Person> getItems() {
+        List<Person> items = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            items.add(new Person(i + 1, "test name" + i, "test age", "test address"));
+        }
+
+        return items;
+    }
+
+
+}
